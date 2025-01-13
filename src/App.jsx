0ba4e295c5f5e50f42c7be34b2b080935d203ee0
cbaltao <cbaltao@rrc.ca>
2025-01-13 16:22:39 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="Carlo" body="Lorem ipsum" />
+      <Post author="Mantis" body="Lorem ipsum" />
+      <Post />
+    </main>
+  );
+}
+
+export default App;
